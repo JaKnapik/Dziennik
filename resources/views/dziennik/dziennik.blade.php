a407c76a97156853c4a19f2617c44251b03ab537
CzornyProrok <maciejkna470@gmail.com>
2019-01-07 18:09:12 +0100

    Nadanie stylu

---
+++ resources/views/dziennik/dziennik.blade.php
@@ -0,0 +1,16 @@
+@extends ('layout')
+
+@section('content')
+
+    <!-- Banner -->
+    <section id="banner">
+        <div class="inner">
+            <h2>Dziennik elektroniczny</h2>
+            <p>Kontroluj swoje oceny<br />
+                <ul class="actions special">
+                <li><a href="login" class="button primary">Zaloguj</a></li>
+            </ul>
+        </div>
+        <a href="#one" class="more scrolly">Learn More</a>
+    </section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+Route::resource('dziennik', 'DziennikController');
+Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
